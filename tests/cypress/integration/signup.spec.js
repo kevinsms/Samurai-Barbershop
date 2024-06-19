865b464ce9ca26a3ea270da841b4f48f3af8532b
Kevin <kevin.mss@hotmail.com>
2024-06-18 22:20:06 -0300

    Iniciando os testes de ponta a ponta (E2E)

--- tests/cypress/integration/signup.spec.js
+++ tests/cypress/integration/signup.spec.js
@@ -1,11 +1,72 @@
-it('deve cadastrar um novo usuário', function (){
-    const name = 'Kevin Silva'
-    const email = 'kevin.silva@samuraibs.com'
-    const password = "pwd123"
+describe('cadastro', ()=>{
 
-    cy.visit('/signup')
+    context('quando o usuário é novato', function (){
+        const user = {
+            name: 'Kevin Silva',
+            email: 'kevin.mss@hotmail.com',
+            password: 'pwd123'
+        }
 
-    cy.get("input[placeholder='Nome']").type(name)
-    cy.get("input[placeholder='E-mail']").type(email)
-    cy.get("input[placeholder='Senha']").type(password)
+        before(function (){
+            cy.task('removeUser',user.email)
+                .then(function (result){
+                    console.log(result)
+                })
+        })
+        it('deve cadastrar com sucesso', function (){
+
+            cy.visit('/signup')
+
+            cy.get("input[placeholder='Nome']").type(user.name)
+            cy.get("input[placeholder='E-mail']").type(user.email)
+            cy.get("input[placeholder='Senha']").type(user.password)
+
+            cy.contains('button','Cadastrar').click()
+
+            cy.get('.toast')
+                .should('be.visible')
+                .find('p')
+                .should('have.text', 'Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!')
+        })
+    })
+
+context('quando o email já existe', function (){
+    const user = {
+        name: 'Kevin Silva2',
+        email: 'kevin.mss2@hotmail.com',
+        password: 'pwd123',
+        is_provider: true
+    }
+
+    before(function (){
+        cy.task('removeUser',user.email)
+            .then(function (result){
+                console.log(result)
+            })
+        cy.request(
+            'POST',
+            'http://localhost:3333/users',
+            user
+        ).then(function (response){
+            expect(response.status).to.eq(200)
+        })
+    })
+    it('não deve cadastrar o usuário', function (){
+        cy.visit('/signup')
+
+        cy.get("input[placeholder='Nome']").type(user.name)
+        cy.get("input[placeholder='E-mail']").type(user.email)
+        cy.get("input[placeholder='Senha']").type(user.password)
+
+        cy.contains('button','Cadastrar').click()
+
+        cy.get('.toast')
+            .should('be.visible')
+            .find('p')
+            .should('have.text', 'Email já cadastrado para outro usuário.')
+    })
+
 })
+
+})
+
--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -12,6 +12,8 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const { Pool } = require('pg')
+
 /**
  * @type {Cypress.PluginConfig}
  */
@@ -19,4 +21,24 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+    const pool = new Pool({
+        host: 'fanny.db.elephantsql.com',
+        user: 'vtldgnsr',
+        password: 'w1zPFKcKSNnPtanb9Ze6-j3_8gUAS85N',
+        database: 'vtldgnsr',
+        port: 5432
+    })
+
+    on('task',{
+        removeUser(email){
+            return new Promise(function (resolve){
+                pool.query('DELETE FROM public.users WHERE email = $1', [email], function(error, result){
+                    if(error){
+                        throw error
+                    }
+                    resolve({success: result})
+                })
+            })
+        }
+    })
 }
