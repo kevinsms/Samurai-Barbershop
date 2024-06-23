3e75fc96b28da783ad823da57096b0f900b5efdc
Kevin <kevin.mss@hotmail.com>
2024-06-23 14:42:15 -0300

    Organizando o código de testes com Page Objects

--- tests/cypress/integration/signup.spec.js
+++ tests/cypress/integration/signup.spec.js
@@ -1,3 +1,5 @@
+import signupPage from '../support/pages/signup'
+
 describe('cadastro', ()=>{
 
     context('quando o usuário é novato', function (){
@@ -15,34 +17,27 @@ describe('cadastro', ()=>{
         })
         it('deve cadastrar com sucesso', function (){
 
-            cy.visit('/signup')
+            signupPage.go()
+            signupPage.form(user)
+            signupPage.submit()
+            signupPage.toast.shouldHaveText("Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!")
 
-            cy.get("input[placeholder='Nome']").type(user.name)
-            cy.get("input[placeholder='E-mail']").type(user.email)
-            cy.get("input[placeholder='Senha']").type(user.password)
-
-            cy.contains('button','Cadastrar').click()
-
-            cy.get('.toast')
-                .should('be.visible')
-                .find('p')
-                .should('have.text', 'Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!')
         })
     })
 
-context('quando o email já existe', function (){
-    const user = {
-        name: 'Kevin Silva2',
-        email: 'kevin.mss2@hotmail.com',
-        password: 'pwd123',
-        is_provider: true
-    }
+    context('quando o email já existe', function (){
+        const user = {
+            name: 'Kevin Silva2',
+            email: 'kevin.mss2@hotmail.com',
+            password: 'pwd123',
+            is_provider: true
+        }
 
-    before(function (){
-        cy.task('removeUser',user.email)
-            .then(function (result){
-                console.log(result)
-            })
+        before(function (){
+            cy.task('removeUser',user.email)
+                .then(function (result){
+                    console.log(result)
+                })
         cy.request(
             'POST',
             'http://localhost:3333/users',
@@ -52,21 +47,77 @@ context('quando o email já existe', function (){
         })
     })
     it('não deve cadastrar o usuário', function (){
-        cy.visit('/signup')
 
-        cy.get("input[placeholder='Nome']").type(user.name)
-        cy.get("input[placeholder='E-mail']").type(user.email)
-        cy.get("input[placeholder='Senha']").type(user.password)
+        signupPage.go()
+        signupPage.form(user)
+        signupPage.submit()
+        signupPage.toast.shouldHaveText("Email já cadastrado para outro usuário.")
 
-        cy.contains('button','Cadastrar').click()
-
-        cy.get('.toast')
-            .should('be.visible')
-            .find('p')
-            .should('have.text', 'Email já cadastrado para outro usuário')
     })
 
 })
 
+    context('quando o email é incorreto', function (){
+        const user = {
+            name: 'Elizabeth Olsen',
+            email: 'liza.yahoo.com',
+            password: 'pwd123',
+        }
+
+        it('deve exibir mensagem de alaerta', function (){
+            signupPage.go()
+            signupPage.form(user)
+            signupPage.submit()
+
+            signupPage.alertHaveText('Informe um email válido')
+        })
+
+    })
+
+    context('quando a senha é muito curto', function (){
+
+        const passwords = ['1','2a','ab3','abc4','ab#c5']
+
+        beforeEach(function (){
+            signupPage.go()
+        })
+
+        passwords.forEach(function (p){
+
+            it('não deve cadastrar com a senha: ' + p, function (){
+
+                const user = {name: 'Jason Friday',email: 'jason@gmail.com',password: p}
+
+                signupPage.go()
+                signupPage.form(user)
+                signupPage.submit()
+            })
+        })
+        afterEach(function (){
+            signupPage.alertHaveText('Pelo menos 6 caracteres')
+        })
+
+    })
+
+    context.only('quando não preencho nenhum dos campos', function (){
+
+        const alertMessages = [
+            'Nome é obrigatório',
+            'E-mail é obrigatório',
+            'Senha é obrigatória'
+        ]
+
+        before(function (){
+            signupPage.go()
+            signupPage.submit()
+        })
+
+        alertMessages.forEach(function (alert){
+            it('deve exibir ' + alert.toLocaleLowerCase(),function (){
+                signupPage.alertHaveText(alert)
+            })
+        })
+
+    })
 })
 
